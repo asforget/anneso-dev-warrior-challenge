737101edf8e445ca48ba01c3c6f0a76af680afc5
Anne Sophie <asforgetleroux@gmail.com>
2021-10-14 13:35:32 -0400

    home

---
+++ script.js
@@ -0,0 +1,4 @@
+const btn = document.querySelectorAll('.btn')
+
+btn.addEventListener('mouseenter', () => container.classList.add('hover'))
+btn.addEventListener('mouseleave', () => container.classList.remove('hover'))
