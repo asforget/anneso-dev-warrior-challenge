f23bbb03804758eda4504140607a02debc3e510e
Anne Sophie <asforgetleroux@gmail.com>
2021-10-14 13:59:39 -0400

    update button

--- script.js
+++
@@ -1,4 +0,0 @@
-const btn = document.querySelectorAll('.btn')
-
-btn.addEventListener('mouseenter', () => container.classList.add('hover'))
-btn.addEventListener('mouseleave', () => container.classList.remove('hover'))
