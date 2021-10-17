bf15be857e43e14625f9460d182f306cb3b4e6ff
Anne Sophie <asforgetleroux@gmail.com>
2021-10-17 19:33:28 -0400

    pokedex progress

--- scroll-animation/script.js
+++ scroll-animation/script.js
@@ -1,19 +1,45 @@
-const boxes = document.querySelectorAll('.box, .img') 
+// const boxes = document.querySelectorAll('.box, .img') 
 
-window.addEventListener('scroll', checkBoxes)
+// window.addEventListener('scroll', checkBoxes)
 
+// checkBoxes()
+
+// function checkBoxes() {
+//     const triggerBottom = window.innerHeight / 5 * 4
+
+//     boxes.forEach(box => {
+//         const boxTop = box.getBoundingClientRect().top 
+
+//         if(boxTop < triggerBottom) {
+//             box.classList.add('show')
+//         } else {
+//             box.classList.remove('show')
+//         }
+//     })
+// }
+
+const boxes = document.querySelectorAll('.box, .img')
+const boxesBounds = []
+const triggerBottom = window.innerHeight / 5 * 4
+
+cacheBoxes()
 checkBoxes()
 
+window.addEventListener('scroll', checkBoxes)
+window.addEventListener('resize', cacheBoxes)
+
 function checkBoxes() {
-    const triggerBottom = window.innerHeight / 5 * 4
-
-    boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top 
-
-        if(boxTop < triggerBottom) {
+    boxes.forEach((box, index) => {
+        if(boxesBounds[index] - window.pageYOffset < triggerBottom) {
             box.classList.add('show')
         } else {
             box.classList.remove('show')
         }
     })
+}
+
+function cacheBoxes() {
+  boxes.forEach(box => {
+    boxesBounds.push(window.pageYOffset + box.getBoundingClientRect().top)
+  })
 }
